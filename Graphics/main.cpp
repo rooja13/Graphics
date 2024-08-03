703e3118a1f241999cb738117d6aa1540a322a89
rooja13 <rbarajas1633@yahoo.com>
2024-08-02 20:42:21 -0700

    Code to open a window Section 4 of learn openGL: Hello Window

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,7 +1,61 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main() {
-	//This is a test comment for a push
+	// initialize glfw
+	glfwInit();
+	
+	//Window hint is configuring the settings our our window
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//create the window object that holds window data
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Window!!!! :)", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window.\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD.\n";
+		return -1;
+	}
+
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//input
+		processInput(window);
+
+		//Rendering commands here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//check and callevents and call buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+//handles resize of window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	//if esc is pressed then close window
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
